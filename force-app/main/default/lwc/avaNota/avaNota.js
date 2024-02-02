be7d76f9a74c32a842d33354e198a760a8189e9c
Lucas Ragazzi Pereira Maciel <lucasax147@gmail.com>
2024-02-02 10:03:24 -0300

    add avatabela

--- force-app/main/default/lwc/avaNota/avaNota.js
+++ force-app/main/default/lwc/avaNota/avaNota.js
@@ -1,13 +1,11 @@
 import { LightningElement, track } from 'lwc';
 import inserirAvaliacao from '@salesforce/apex/AvaController.inserirAvaliacao';
 
-
 export default class AvaNota extends LightningElement {
     @track titulo = '';
     @track descricao = '';
     @track value = '';
 
-    
     get options() {
         return [
             { label: '1', value: '1' },
@@ -15,16 +13,14 @@ export default class AvaNota extends LightningElement {
             { label: '3', value: '3' },
             { label: '4', value: '4' },
             { label: '5', value: '5' },
-        ]
+        ];
     }
 
-    handleChange(event){
+    handleChange(event) {
         this.value = event.detail.value;
-        
     }
 
     contactChangeVal(event) {
-       
         if (event.target.name === 'titulo') {
             this.titulo = event.target.value;
         } else if (event.target.name === 'descricao') {
@@ -32,18 +28,19 @@ export default class AvaNota extends LightningElement {
         }
     }
 
-    // Chame o método Apex para inserir os dados
     handleClick() {
-        
         inserirAvaliacao({ titulo: this.titulo, descricao: this.descricao, nota: this.value })
             .then(result => {
-                
                 console.log('Inserção bem-sucedida:', result);
+
+                // Emitir um evento personalizado para notificar outros componentes
+                const successEvent = new CustomEvent('avaliacaoadicionada', {
+                    detail: result
+                });
+                this.dispatchEvent(successEvent);
             })
             .catch(error => {
-              
                 console.error('Erro ao inserir:', error);
             });
     }
 }
-
